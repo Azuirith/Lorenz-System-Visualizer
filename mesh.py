b5c98c61c246256a6b3cea047d44ef0e0edefd0c
Rowan Segner <91701046+Azuirith@users.noreply.github.com>
2025-07-18 13:23:45 -0400

    Delete mesh.py

--- mesh.py
+++
@@ -1,7 +0,0 @@
-from triangle import Triangle
-
-class Mesh:
-    def __init__(self, size: int, color: tuple) -> None:
-        self.triangles: list[Triangle] = []
-        self.size = size
-        self.color = color
