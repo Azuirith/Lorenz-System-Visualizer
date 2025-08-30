3e3e3cf3f1420a076958afbfbe5bc83116d3b09d
Azuirith <azuirithdev@gmail.com>
2025-08-30 03:46:09 +0000

    Reverting things because my code wasn't working

--- triangle.py
+++
@@ -1,5 +0,0 @@
-from pygame import Vector3
-
-class Triangle:
-    def __init__(self, vertex_0: Vector3, vertex_1: Vector3, vertex_2: Vector3) -> None:
-        self.vertices: list[Vector3] = [vertex_0, vertex_1, vertex_2]
